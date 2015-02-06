854cb4d922e537f0399a6c6164757b6d8f9f9c3a
Henry Crute <hcrute@seads-db.soe.ucsc.edu>
2015-02-05 16:32:14 -0800

    Added the plug emulator project, TODO: create a python hierarchy to handle it, instead of a shell script

---
+++ plug-emulator/insert.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# this script inserts dummy data into the databse
+# acts as a pseudo shemp
+
+#header - L1CTHS111111t00000000000000X
+#sample - L2CTWlIt00000000000100P00000000000005C01D01X
+
