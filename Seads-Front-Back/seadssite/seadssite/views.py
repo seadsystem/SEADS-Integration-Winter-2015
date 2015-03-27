23e459934e9f48c183ca8f5b8649c894ac43aaac
Dickson Tong <dtong4@ucsc.edu>
2015-03-26 21:44:14 -0700

    controller changed for list.html

--- Seads-Front-Back/seadssite/seadssite/views.py
+++ Seads-Front-Back/seadssite/seadssite/views.py
@@ -215,24 +215,4 @@ def VisualizationView(request, device_id):
 
 
 def list(request):
-    # Handle file upload
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile = request.FILES['docfile'])
-            newdoc.save()
-
-            # Redirect to the document list after POST
-            return HttpResponseRedirect(reverse('SeadsFront.seadssite.views.list'))
-    else:
-        form = DocumentForm() # A empty, unbound form
-
-    # Load documents for the list page
-    documents = Document.objects.all()
-
-    # Render list page with the documents and the form
-    return render_to_response(
-        'list.html',
-        {'documents': documents, 'form': form},
-        context_instance=RequestContext(request)
-    )
+	return render_to_response('list.html')
